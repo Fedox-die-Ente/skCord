5a35535723d8abb9feb27a1d35aed36e9895a82a
Fedox <mrminecrafttv19@gmail.com>
2024-04-27 02:40:21 +0200

    version

--- src/main/java/me/fedox/skcord/commands/CommandSkcord.java
+++ src/main/java/me/fedox/skcord/commands/CommandSkcord.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 public class CommandSkcord implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        commandSender.sendMessage("This server is running skCord v2.1");
+        commandSender.sendMessage("This server is running skCord v2.2");
         commandSender.sendMessage("skCord is a plugin that allows you to connect your Minecraft server to your Discord server via webhooks.");
         return false;
     }
